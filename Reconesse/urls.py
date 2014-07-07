83d7c50890ff226483375cfc60a93638fc48c844
David Gundry <davidgundry@outlook.com>
2014-07-07 14:40:23 +0100

    Adding simple index.html

--- Reconesse/urls.py
+++ Reconesse/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,6 +9,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'Reconesse.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name="home"),
 	url(r'^article/', include('past.urls')),
 	url(r'^past/', include('past.urls')),
     url(r'^admin/', include(admin.site.urls)),
