b5a2e61a7f5de6249b619646bc7d0eeff5a9de92
Kristján Eldjárn <kristjan@eldjarn.net>
2014-09-09 15:54:10 +0000

    Tabs -> spaces in past/models.py

--- past/models.py
+++ past/models.py
@@ -7,46 +7,47 @@ import urlutils
 
 
 class CategoryManager(models.Manager):
-	def GetRoots(self):
-		topLevel = self.filter(parent__isnull=True)
-		return topLevel
+    def GetRoots(self):
+        topLevel = self.filter(parent__isnull=True)
+        return topLevel
 
 
-	def GetTree(self):
-		roots = self.GetRoots()
+    def GetTree(self):
+        roots = self.GetRoots()
 
-		tree = []
+        tree = []
 
-		for root in roots:
-			children = self.filter(parent__id=root.id)
-			branch = [root, children]
-			tree.append(branch)
+        for root in roots:
+            children = self.filter(parent__id=root.id)
+            branch = [root, children]
+            tree.append(branch)
 
-		return tree
+        return tree
 
 
-	def GetOrdered(self):
-		tree = self.GetTree()
-		ordered = []
-		for branch in tree:
-			ordered.append(branch[0])
-			ordered += branch[1]
+    def GetOrdered(self):
+        tree = self.GetTree()
+        ordered = []
+        for branch in tree:
+            ordered.append(branch[0])
+            ordered += branch[1]
 
-		return ordered
+        return ordered
 
 
 class Category(models.Model):
-	name = models.CharField(max_length=200)
-	parent = models.ForeignKey("self", null=True, blank=True, related_name="parentcategory")
+    name = models.CharField(max_length=200)
+    parent = models.ForeignKey("self", null=True, blank=True, related_name="parentcategory")
 
-	objects = CategoryManager()
+    objects = CategoryManager()
 
-	def __unicode__(self):
-		return self.name
+    def __unicode__(self):
+        return self.name
 
 
 class Article(models.Model):
-	class Meta:
+
+    class Meta:
         ordering = ('reference',)
 
     # reference contains a unique reference string for the article
@@ -57,29 +58,29 @@ class Article(models.Model):
                                  default = None,
                                  unique = True)
 
-	title = models.CharField(max_length=200)
-	content = models.TextField()
-	summaryLines = models.TextField()
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    summaryLines = models.TextField()
 
-	birthYear = models.IntegerField(default=0);
-	deathYear = models.IntegerField(null=True, blank=True);
+    birthYear = models.IntegerField(default=0);
+    deathYear = models.IntegerField(null=True, blank=True);
 
-	country = CountryField()
+    country = CountryField()
 
-	relatedArticles = models.ManyToManyField("self", related_name="relatedby")
+    relatedArticles = models.ManyToManyField("self", related_name="relatedby")
 
-	category = models.ForeignKey(Category, related_name="category")
-	tags = TaggableManager(related_name="past_tags")
+    category = models.ForeignKey(Category, related_name="category")
+    tags = TaggableManager(related_name="past_tags")
 
-	def __unicode__(self):
-		return self.title
+    def __unicode__(self):
+        return self.title
 
-	def getTagNames(self):
-		return self.tags.all()
+    def getTagNames(self):
+        return self.tags.all()
 
-	def save(self, *args, **kwargs):
-		""" We redefine the save method to create a unique reference number upon
-		being called for the first time on any Article """
+    def save(self, *args, **kwargs):
+        """ We redefine the save method to create a unique reference number upon
+        being called for the first time on any Article """
         created = not self.pk
         super(Article, self).save(*args, **kwargs)
         if created:
@@ -88,8 +89,8 @@ class Article(models.Model):
 
 
 class PastImage(models.Model):
-	imageField = models.ImageField(upload_to="images")
-	article = models.ForeignKey(Article)
+    imageField = models.ImageField(upload_to="images")
+    article = models.ForeignKey(Article)
 
-	def __unicode__(self):
-		return "Article: '" + self.article.title + "' - Image: " + self.imageField.url
+    def __unicode__(self):
+        return "Article: '" + self.article.title + "' - Image: " + self.imageField.url
