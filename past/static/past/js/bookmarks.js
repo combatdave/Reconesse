47bb6bc17cf08b523d4687e99912a91dbee157dd
Kristján Eldjárn <kristjan@eldjarn.net>
2014-09-21 11:33:36 +0000

    Added foundation for bookmarks using localstorage

---
+++ past/static/past/js/bookmarks.js
@@ -0,0 +1,46 @@
+var storage = {
+    bookmarks = [],
+    support: function()
+    {
+        /* Check whether browser supports localStorage */
+        try
+        {
+            return 'localStorage' in window && window['localStorage'] !== null;
+        }
+        catch (e)
+        {
+            return false;
+        }
+    }
+}
+
+
+function renderBookmark(bookmark, template, node)
+{
+
+}
+
+function renderBookmarkArray(bookmarks, template, node)
+{
+    node.empty();
+    for (var i = 0; i < bookmarks.length; ++i)
+    {
+        renderBookmark(bookmarks[i],
+                       template,
+                       node)
+    }
+}
+
+$(document).ready(function(e)
+{
+    if (storage.support())
+    {
+        // If localStorage is supported by client browser
+        if (!localStorage['bookmarks']) localStorage['bookmarks'] = [];
+        storage.bookmarks = json.parse(localStorage['bookmarks']);
+        //renderBookmarkArray(storage.bookmarks)
+    }
+
+
+
+}
