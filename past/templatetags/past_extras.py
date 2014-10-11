c87a69f4af4ca1403d46268d1aab3e586a966a69
combatdave <davidgundry@outlook.com>
2014-10-11 19:47:55 +0000

    Fixing a unicode bug caused by a print

--- past/templatetags/past_extras.py
+++ past/templatetags/past_extras.py
@@ -10,9 +10,7 @@ register = template.Library()
 def addReferences(text):
     """Turns all instances of [1] into the link to that reference"""
     regexMatch = re.compile(r'(\[\d+\])')
-    print text
     for match in regexMatch.findall(text):
-        print match
         id = match.replace("[", "").replace("]", "")
 
         link = "<a href=#reference-{id}><sup>[{id}]</sup></a>".format(id=id)
